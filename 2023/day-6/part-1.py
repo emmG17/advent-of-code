9396eb71f8852d882a955dbb64e5f39eb735130d
Emmanuel Gonzalez <emm.gs17@gmail.com>
2023-12-10 23:45:49 -0600

    Day 6 - Wait For It (#6)

---
+++ 2023/day-6/part-1.py
@@ -0,0 +1,35 @@
+import math
+
+def parse_file(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def data_table(filename):
+    time, distance = parse_file(filename) 
+    time = list(map(int, time.split(':')[1].split()))
+    distance = list(map(int, distance.split(':')[1].split()))
+    return zip(time, distance)
+
+def chicharronera(a, b, c) -> tuple[float, float]:
+    # This is the general formula for a quadratic equation, it is adjusted to pick discrete values
+    big_sqrt = math.sqrt(b**2 - 4*a*c)
+    first = math.ceil((-b + big_sqrt)/(2*a))
+    second = math.floor((-b - big_sqrt)/(2*a))
+    return first, second
+
+# 'tis but a parable
+def parable_intersect(time, distance):
+    # the parable formula is -x^2 + tx where t is the max time and x is the time ellapsed
+    a = -1
+    b = time
+    c = -(distance + 1)
+    first, last = sorted(chicharronera(a, b, c))
+    return (last - first) + 1
+
+data = list(data_table('input.txt'))
+multiplication = 1
+for t, d in data:
+    multiplication *= parable_intersect(t, d)
+print(multiplication)
